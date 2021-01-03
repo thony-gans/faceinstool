f5ee809f1ddc6401d8b1c7c077c9ccee7bf22735
Thony-gans <75247911+thony-gans@users.noreply.github.com>
2021-01-04 01:28:55 +0700

    Add files via upload

---
+++ pbhinstool.py
@@ -0,0 +1,439 @@
+#python 2.7.12
+
+#~noRecordtinggalmakeanjinggakpakerepot
+#( information_hacker_tools )
+#*****
+
+h='\033[32;1m'
+m='\033[31;1m'
+p='\033[37;1m'
+y='\033[33;1m'
+
+import os
+import sys
+
+
+os.system('setterm --bold on')
+os.system('setterm --foreground white')
+os.system('pip install -r requirements.txt')
+print ""
+print '''\033[32;1m
+
+   Creator by @.
+  ____  ____  ______  ____  ____  _________
+  mmmm\.mmmm\.mmmmmm\.mmmm\.mmmm\.mmmmmmmmm\...
+   mm\...mm\..mm\...\..mm\...mm\..mmm\.....\...
+   mmmmmmm\...mmmmmm\..mmmmmmmm ..mmmmmmmmm\...
+   mm\...mm\..mm\...\.....mm\..\..\..../mmm ...
+  mmmm..mmmm\.mmmmmm\....mmmm\....mmmmmmmmm\...
+  \...\.\...\.\.....\....\...\....\.......\....'''
+
+print '''
+\033[33;1m [\033[31;1m+\033[33;1m]=======================[\033[36;1m+\033[33;1m]
+
+\033[31;1m  01)\033[32;1m hack website   \033[33;1m(ok)
+                         
+\033[31;1m  02)\033[32;1m wifi spy       \033[33;1m(ok)
+                      
+\033[31;1m  03)\033[32;1m Spam target    \033[33;1m(ok)
+                       
+\033[31;1m  04)\033[32;1m Cracking       \033[33;1m(ok)
+
+\033[31;1m  05)\033[32;1m exploit shell  \033[33;1m(ok)
+
+\033[31;1m  06)\033[32;1m phishing       \033[33;1m(ok)
+
+\033[31;1m  07)\033[32;1m maining BTC    \033[33;1m(ok)
+
+\033[31;1m  08)\033[32;1m Virus          \033[33;1m(ok)
+
+\033[31;1m  09)\033[32;1m info script    \033[33;1m(ok)
+
+\033[31;1m  00)\033[37;1m close          \033[33;1m(ok)
+\033[33;1m_
+\033[33;1m [\033[37;1m+\033[33;1m]=======================[\033[35;1m+\033[33;1m]'''
+print "__________||"
+print "| _________|"
+print "||"
+print "||"
+print "||_____"
+key = raw_input("|_____(>\033[33;1mwho\033[37;1m@\033[31;1mkeysexploit\033[36;1m ~# ")
+                                       
+if key =="01":
+    print "\033[37;1m =========================="
+    print ""
+    print " x1) Scan Vulnerability"
+    print ""
+    print " x2) Exploit vuln Sql"
+    print ""
+    print " x3) admin finder"
+    print ""
+    print " x4) Webdav"
+    print ""
+    print " x5) wordpress scan"
+    print ""
+    print " x6) rescript deface html"
+    print ""
+    print " x7) XSS strike tool"
+    print ""
+    print " hx) CODE HASHER"
+    print ""
+    print " =========================="
+    print ""
+
+elif key =="02":
+    print "\033[37;1m ========================="
+    print ""
+    print " x8) Spoofing Dns"
+    print ""
+    print " x9) Cracking wifi"
+    print ""
+    print " 1x0) scanning target"
+    print ""
+    print " 1x1) telnet"
+    print ""
+    print " 1x2) Wifiphisher"
+    print "" 
+    print " =========================="
+                                    
+elif key =="03":
+    print "\033[37;1m ========================="
+    print ""   
+    print " 1x3) Spam Emails"
+    print ""
+    print " 1x4) Spam Sms"
+    print ""
+    print " 1x5) Dos Attack"
+    print ""
+    print " 1x6) Spam WhatsApp"
+    print ""
+    print " =========================="
+
+elif key =="04":
+    print "\033[37;1m ========================"
+    print ""
+    print " 1x7) Cracking akun FB"
+    print ""
+    print " 1x8) Cracking akun IG"
+    print ""
+    print " 1x9) Open-Source facebook"
+    print ""
+    print " 2x0) Cracking all Hack Server"
+    print ""
+    print " =========================="
+
+
+elif key =="05":
+    print "\033[37;1m ========================"
+    print ""   
+    print " 2x1) Social Engineering"
+    print ""
+    print " 2x2) Metasploit"
+    print ""
+    print " 2x3) Routersploit"
+    print ""
+    print " =========================="
+
+elif key =="06":
+    print "\033[37;1m ========================"
+    print ""
+    print " 2x4) Phishing all medsos"
+    print ""
+    print " 2x5) Phishing Game"
+    print ""
+    print " 2x6) Fake Send Email msg"
+    print ""
+    print " 2x7) UserRecon"
+    print ""
+    print " 2x8) Tracking Ip"
+    print ""
+    print " =========================="
+
+                                
+elif key =="07":
+    print ""
+    print "\033[37;1m ======================="
+    print ""
+    print " 2x9) Miner Telegram"
+    print ""
+    print " 3x0) Miner Webinstall"
+    print ""
+    print " 3x1) Monitor miner apk"
+    print ""
+    print " =========================="
+elif key =="08":
+    print ""
+    print " 3x2) Bug Maker tools"
+    print ""
+    print " =========================="
+
+elif key =="09":
+    print "Anda akan segera mendapatkan info tentang script ini"
+    print "sabar.."
+    os.system('sleep 5')
+
+elif key =="00":
+    os.system('exit')
+
+id = raw_input("\033[37;1m[\033[36;1m*\033[37;1m] action ~> ")
+if id =="x1":
+    print "[*] Progress_main.."
+    os.system('git clone https://github.com/Gameye98/OWScan')
+    print "\033[33;1m[*] ""\033[32;1mPerfect_installation.."
+    os.system('pkg install php')
+    os.system('pkg install git')
+    os.system('cd OWScan')
+    print "\033[31;1m[!] installation success.."
+    os.system('sleep 2')
+    os.system('php owscan.php')
+       
+elif id =="x2":
+    print "[*] Progress_main.."
+    os.system('git clone https://github.com/sqlmapproject/sqlmap.git')
+    print "\033[33;1m[*] ""\033[32;1mPerfect_installation.."
+    os.system('pkg install git')
+    print "\033[31;1m[!] installation success.."
+    os.system('python2 sqlmap.py')
+
+elif id =="x3":
+    print "[*] Progress_main.."
+    os.system('git clone https://github.com/mdpunk/admin-finder')
+    print "\033[33;1m[*] ""\033[32;1mPerfect_installation.."
+    os.system('cd admin-finder')
+    os.system('pkg install php')
+    os.system('chmod +x adfin.php')
+    print "\033[31;1m[!] installation success.."
+    os.system('sleep 2')
+    os.system('php adfin.php')
+            
+elif id =="x4":
+    print "[*] Progress_main"
+    os.system('apt install libcurl')
+    os.system('apt install wget')
+    os.system('pip2 install urllib3 chardet certifi idna requests')
+    os.system('git clone https://github.com/B41B4L/WEBDAV')
+    print "\033[33;1m masukan perintah :\033[32;1m cd WEBDAV"
+    print "\033[33;1m masukan perintah :\033[32;1m sh bahan.sh"
+    print "\033[33;1m masukan perintah :\033[32;1m sh dav.sh"
+    print "\033[31;1m jangan malas kambing:v"            
+elif id =="x5":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/m4ll0k/WPSeku.git')
+    print "\033[33;1m masukan perintah :\033[32;1m cd WPSeku"
+    print "\033[33;1m masukan perintah :\033[32;1m python2 wpseku.py"
+    print "\033[31;1m jangan malas kambing:v"
+
+elif id =="x6":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/4L13199/LITESCRIPT')
+    print "\033[33;1m masukan perintah :\033[32;1m cd LITESCRIPT"
+    print "\033[33;1m masukan perintah :\033[32;1m python2 LITESCRIPT.py"
+    print "\033[31;1m jangan malas kambing:v"
+            
+elif id =="x7":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/s0md3v/XSStrike')
+    os.system('cd XSStrike')
+    os.system('pip install -r requirements.txt')
+    os.system('python xsstrike.py')
+    print "\033[33;1m masukan perintah :\033[32;1m cd XSStrike"
+    print "\033[33;1m masukan perintah :\033[32;1m python2 xsstrike.py"
+    print "\033[31;1m jangan malas kambing:v"
+
+elif id =="hx":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/ciku370/hasher')
+    print "\033[33;1m masukan perintah :\033[32;1m cd hasher"
+    print "\033[33;1m masukan perintah :\033[32;1m python2 hash.py"
+    print "\033[31;1m jangan malas kambing:v"
+
+#~Networking
+
+elif id =="x8":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('https://github.com/Trackbool/DerpNSpoof')
+    print "\033[33;1m masukan perintah :\033[32;1m cd DerpNSpoof"
+    print "\033[33;1m masukan perintah :\033[32;1m python3 DerpNSpoof.py"
+
+elif id =="x9":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https//github.com/aircrack-ng/aircrack-ng.git')
+    os.system('cd aircrack-ng')
+    os.system('apt-get install libssl-dev libpcre3-dev libsqlite-dev libnl-3-dev libnl-genl-3-dev pkg-config-y')
+    os.system('make install')
+    os.system('aircrack-ng')
+
+elif id =="1x0":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('apt install nmap')
+    os.system('nmap')
+
+elif id =="1x1":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('apt-get install telnetd')
+    os.system('telnet --help')
+    os.system('telnet')
+
+elif id =="1x2":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/wifiphisher/wifiphisher.git')
+    os.system('cd wifiphisher')
+    os.system('python setup.py install')
+
+#~spamner
+
+
+elif id =="1x3":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/revan-ar/mail-spammer')
+    os.system('cd mail-spammer')
+    os.system('php mail.php')
+               
+elif id =="1x4":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/kumpulanremaja/spam-sms.git')
+    os.system('cd spam-sms')
+    os.system('bash spam.sh')
+
+elif id =="1x5":
+    print "\033[33;1m[*] Progress_main.."
+    os.system(' git clone https://github.com/4L13199/LITEDDOS')
+    os.system('cd LITEDDOS.py')
+    os.system('python2 LITEDDOS.py')
+
+elif id =="1x6":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/kumpulanremaja/spam-wa.git')
+    os.system('cd spam wa')
+    os.system('php wa.php')
+
+#cracking
+
+elif id =="1x7":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/noolep/multiBF_ig')
+    os.system('cd multiBF_ig')
+    os.system('apt install tor')
+    os.system('apt install jq')
+    os.system('apt install bash')
+    os.system('tor')
+    os.system('bash brute.sh')
+
+elif id =="1x8":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/BOT-033/Sensei')
+    os.system('cd sensei')
+    os.system('pip2 install tqdm')
+    os.system('pip2 install requests')
+    os.system('pip2 install mechanize')
+    os.system('python2 main.py')
+              
+elif id =="1x9":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/CiKu370/OSIF')
+    os.system('cd OSIF')
+    os.system('pip2 install requirements.txt')
+    os.system('python2 osif.py')
+
+elif id =="2x0":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/1N3/BruteX')
+    os.system('cd BruteX')
+    os.system('chmod +x install.sh')
+    os.system('./install.sh')
+
+elif id =="2x1":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/trustedsec/social-engineer-toolkit/setoolkit/')
+    os.system('cd setoolkit')
+    os.system('pip3 install -r requirements.txt')
+    os.system('python setup.py')
+
+elif id =="2x2":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('apt install curl')
+    os.system('curl -LO https://raw.githubusercontent.com/Techzindia/Metasploit_For_Termux/master/metasploitTechzindia.sh')
+    os.system('sh metasploitTechzindia.sh')
+    os.system('cd metasploit-framework')
+    os.system('./msfconsole')
+
+elif id =="2x3":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/reverse-shell/routersploit.git')
+    os.system('cd routersploit')
+    os.system('pip install -r requirements.txt')
+    os.system('termux-fix-shebang rsf.py')
+    os.system('./rsf.py')
+
+
+#phishing
+
+
+elif id =="2x4":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('apt install wget')
+    os.system('apt install ssh')
+    os.system('apt install php')
+    os.system('git clone https://github.com/xHak9x/SocialPhish')
+    os.system('ls')
+    print "\033[31;1m[!] ketikan perintah chmod +x *"
+    print "\033[31;1m[!] Ketikan Perintah ./SocialPhish.sh"
+                                                                
+elif id =="2x5": 
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/ridwanirawan/PiPhish')
+    os.system('apt install ruby')
+    os.system('apt install figlet')
+    os.system('apt install toilet')
+    os.system('apt install bash')
+    print "\033[31;1m [!] Silahkan masuk ke Directory untuk menjalankan.."
+    os.system('ls')
+
+elif id =="2x6":
+    print "\033[33:1m[*] Progress_main.."
+    os.system('git clone https://github.com/htr-tech/fake-mailer')
+    os.system('cd fake-mailer')
+    os.system('bash setup.py')
+    os.system('python2 mailer.py')
+
+elif id =="2x7":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/thelinuxchoice/userrecon')
+    os.system('cd userrecon')
+    os.system('bash userrecon.sh')
+
+elif id =="2x8":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('git clone https://github.com/maldevel/IPGeoLocation')
+    os.system('pip3 install -r requirements.txt')
+    print "\033[31;1m [!] silahkan masuk ke Directory untuk menjalankan.."
+    os.system('ls')
+
+#Miner_Script
+
+
+elif id =="2x9":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('termux-open https://t.me/BitcoinClick_bot?start=REpt')
+
+elif id =="3x0":
+    print "\033[33;1m[*] Progress_main.."
+    os.system('termux-open https://cryptobrowser.site/en/login/?next=/en/cryptotab/')
+
+elif id =="3x1":
+    print "\033[32;1m[*] Progress_main.."
+    os.system('termux-open https://m.apkpure.com/id/bitcoin-ticker-widget/st.brothas.mtgoxwidget')
+
+#virus
+
+elif id =="3x2":
+    print "\033[32;1m[*] Waiting.."
+    os.system('termux-setup-storage')
+    os.system('git clone https://github.com/Gameye98/vbug')
+    os.system('cd vbug')
+    os.system('python2 vbug.py')
+
+#!intinya gini bro ane sebenarnya gk niat buat tool
+#!dah lah klo mau recode silahkan
+#!orang gampang aje pake recode segala bro2
+#!yaudah pokoknya semua isi nya gk ada yg niat wkwk
